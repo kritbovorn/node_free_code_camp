675d3515db011458b91bf35992dc6a9bbd64dd74
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-28 00:10:13 +0700

    1-intro.js

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+const amount = 12;
+const num = 14
+
+if (amount < num) {
+    console.log( amount + ' is Small Number');
+} else {
+    console.log( amount + ' is Larger Number');
+}
+
+console.log(` ${ amount } ### Hey it's my first node app !!!.`);
