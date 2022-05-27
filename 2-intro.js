66f83429b7d5bad41c04e2b5ca6fe6db97546545
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-28 00:49:01 +0700

    2-intro.js

---
+++ 2-intro.js
@@ -0,0 +1,13 @@
+// GLOBALS - NO WINDOW !!!
+
+//  __dirname    - path to current Directory
+//  __filename    - file name
+//   require      - function to import modules (ComminJS)
+//   module       - info about current module ( in file )
+//   process      - info about environment where the program is being executed
+
+console.log(__dirname);
+
+setInterval(() => {
+    console.log('Hello World!!!');
+}, 1000);
