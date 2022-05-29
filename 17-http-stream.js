cdacd7868b0dab02f87e20d15f397f1ca61326f2
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-29 21:11:25 +0700

    17-http-stream.js

---
+++ 17-http-stream.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    const text = fs.readFileSync('./content/big.txt', 'utf8');
+    res.end(text);
+}).listen(5000);
