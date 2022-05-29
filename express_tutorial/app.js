27317355bd52a5261ca6754587792f631961453f
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-30 00:44:27 +0700

    add navbar-app folder for html, css and some options

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -1,3 +1,28 @@
 // https://github.com/john-smilga/node-express-course
 
-console.log('Express Tutorial');
+
+const http = require('http');
+const { readFileSync } = require('fs');
+
+// Get all files
+const homePage = readFileSync('./index.html');
+
+
+// Create Server
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write(homePage);
+        res.end();
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write('<h1>About Page</h1>');
+        res.end();
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html' });
+        res.write('<h1>404 Page not founded</h1>');
+        res.end();
+    }
+
+}).listen(5000);
