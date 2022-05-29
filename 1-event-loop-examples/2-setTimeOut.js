c298f81cd89f3b968231fa213545a04456f66337
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-29 12:08:15 +0700

    2-setTimeOut.js

---
+++ 1-event-loop-examples/2-setTimeOut.js
@@ -0,0 +1,10 @@
+// Start operating system process
+
+console.log('First');
+
+setTimeout(() => {
+    console.log('setTimeOut(){} : Second');
+}, 0);
+
+console.log('Third');
+// completed and exited operating system process
