3d130a82a46420ab9e7016f9a763566a24446058
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-30 09:55:04 +0700

    basic show html page by use express

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -1,2 +1,23 @@
 // https://github.com/john-smilga/node-express-course
+// use Express 
+// use Path
 
+const express = require('express');
+const path = require('path');
+
+const app = express();
+// ### Create Public folder
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+});
+
+
+app.all('*', (req, res) => {
+    res.status(404).send('Resource not founded');
+})
+
+app.listen(5000, () => {
+    console.log('Server is listening on PORT: 5000');
+});
