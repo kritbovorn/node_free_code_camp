f8bef9f54e1acf9c7ba9ac318f764fca92c52a1b
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-30 10:25:06 +0700

    another method : by move index.html to public/ folder

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -6,18 +6,26 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-// ### Create Public folder
-app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-});
+
+// ###Create Public folder
+app.use(express.static('./public'));
+app.use(express.static('./public/html'));
+
+
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+// });
 
 
 app.all('*', (req, res) => {
     res.status(404).send('Resource not founded');
 })
 
+
 app.listen(5000, () => {
     console.log('Server is listening on PORT: 5000');
 });
+
