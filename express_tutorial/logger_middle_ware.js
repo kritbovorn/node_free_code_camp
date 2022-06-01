6297f0ec1a2f2591e148e7d5bd8888ba7dbb6526
Kritbovorn <ktaweeyossak@yahoo.com>
2022-06-01 23:32:48 +0700

    6:24:35: 09-middleware-basic.js

---
+++ express_tutorial/logger_middle_ware.js
@@ -0,0 +1,13 @@
+
+const loggerMiddleWare = (req, res, next) => {
+    
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear();
+
+    console.log({method, url, time});
+
+    next();
+}
+
+module.exports = loggerMiddleWare;
